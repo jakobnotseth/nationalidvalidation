b25dcf38e5ec019c93b7fe73c7281f8f4ed41ee0
Jakob Nøtseth <jakob.notseth@evry.com>
2014-12-17 15:37:18 +0100

    Added NHS number validation

---
+++ NationalIdValidation/NationalIdValidation/EnglishNationalInsuranceId.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NationalIdValidation
+{
+    /// <summary>
+    /// Class for input validating National Insurance numbers from United Kingdom
+    /// </summary>
+    public class EnglishNationalInsuranceId
+    {
+        /// <summary>
+        /// Returns whether the string input was validated
+        /// </summary>
+        public bool IsValid { get; private set; }
+
+        public DayOfWeek BenefitsDay { get; private set; }
+
+        /// <summary>
+        /// Creates a EnglishNationalInsuranceId object
+        /// </summary>
+        /// <param name="nationalInsuranceString">Any national insurance number (NINO) string with or without divider (space) between groups</param>
+        /// <example><code>
+        /// var id = new EnglishNationalInsuranceId("AB 12 34 56 C");
+        /// if (id.IsValid) {
+        ///     Console.WriteLine("The NINO number validates");
+        ///     Console.WriteLine(string.Format("This number is payable for social benefits on day {0} of the week", id.BenefitsDay))
+        /// }
+        /// </code></example>
+        public EnglishNationalInsuranceId(string nationalInsuranceString)
+        {
+            IsValid = false;
+            if (string.IsNullOrEmpty(nationalInsuranceString)) return;
+            var reg = Regex.Match(nationalInsuranceString, @"^[ABCEGHJKLMNOPRSTWXYZ][A-NP-Z] ?\d{2} ?\d{2} ?(?<bDay>\d{2}) ?[ABCD]$", RegexOptions.CultureInvariant | RegexOptions.Singleline);
+            if (!reg.Success) return;
+            var benefitsDay = int.Parse(reg.Groups["bDay"].Value);
+            if (benefitsDay <= 19)
+                    BenefitsDay = DayOfWeek.Monday;
+            else if (benefitsDay >= 20 && benefitsDay <= 39)
+                BenefitsDay = DayOfWeek.Tuesday;
+            else if (benefitsDay >= 40 && benefitsDay <= 59)
+                BenefitsDay = DayOfWeek.Wednesday;
+            else if (benefitsDay >= 60 && benefitsDay <= 79)
+                BenefitsDay = DayOfWeek.Thursday;
+            else if (benefitsDay >= 80 && benefitsDay <= 99)
+                BenefitsDay = DayOfWeek.Friday;
+            IsValid = true;
+        }
+    }
+}
---
+++ NationalIdValidation/NationalIdValidation/NhsNumber.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NationalIdValidation
+{
+    /// <summary>
+    /// Class for input validating NHS numbers
+    /// </summary>
+    public class NhsNumber
+    {
+        /// <summary>
+        /// Returns whether the string input was validated
+        /// </summary>
+        public bool IsValid { get; private set; }
+
+        /// <summary>
+        /// Creates a NhsNumber object
+        /// </summary>
+        /// <param name="nhsNumberString">Any NHS number string with or without divider (dash) between groups</param>
+        /// <example><code>
+        /// var id = new NhsNumber("401-023-2137");
+        /// if (id.IsValid) {
+        ///     Console.WriteLine("The NHS number validates");
+        /// }
+        /// </code></example>
+        public NhsNumber(string nhsNumberString)
+        {
+            IsValid = false;
+            if (string.IsNullOrEmpty(nhsNumberString)) return;
+            var reg = Regex.Match(nhsNumberString, @"^(?<d1>\d)(?<d2>\d)(?<d3>\d)-?(?<d4>\d)(?<d5>\d)(?<d6>\d)-?(?<d7>\d)(?<d8>\d)(?<d9>\d)(?<c1>\d)$", RegexOptions.CultureInvariant | RegexOptions.Singleline);
+            if (!reg.Success) return;
+            var d1 = int.Parse(reg.Groups["d1"].Value); // digit 1
+            var d2 = int.Parse(reg.Groups["d2"].Value); // digit 1
+            var d3 = int.Parse(reg.Groups["d3"].Value); // digit 1
+            var d4 = int.Parse(reg.Groups["d4"].Value); // digit 1
+            var d5 = int.Parse(reg.Groups["d5"].Value); // digit 1
+            var d6 = int.Parse(reg.Groups["d6"].Value); // digit 1
+            var d7 = int.Parse(reg.Groups["d7"].Value); // digit 1
+            var d8 = int.Parse(reg.Groups["d8"].Value); // digit 1
+            var d9 = int.Parse(reg.Groups["d9"].Value); // digit 1
+            var c1 = int.Parse(reg.Groups["c1"].Value); // control 1
+            if (d1 == d2 && d2 == d3 && d3 == d4 && d4 == d5 && d5 == d6 && d6 == d7 && d7 == d8 && d8 == d9) return;
+            var remainder = ((d1*10) + (d2*9) + (d3*8) + (d4*7) + (d5*6) + (d6*5) + (d7*4) + (d8*3) + (d9*2)%11);
+            var control = 11 - remainder;
+            if (control == 10) return;
+            if (control == 11) control = 0;
+            if (c1 != control) return;
+            IsValid = true;
+        }
+    }
+}
